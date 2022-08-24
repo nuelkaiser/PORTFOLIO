5f312d46f9f63fcd7a9e96feac41d217fcc75151
nuelKaiser <majekodunmiimmanuel9@gmail.com>
2022-08-24 01:53:52 +0100

    adjustment made by removing get help screen

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import './App.css';
-import { Routes, Route } from "react-router-dom";
-import Gethelp from './Portfolio/Gethelp';
 import Intro from './Portfolio/Intro';
 import Works from './Portfolio/Works';
 import Skillset from './Portfolio/Skillset';
@@ -11,14 +9,13 @@ import Me from './Portfolio/Me';
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Gethelp />} />
-        <Route path="Intro" element={<Intro />} />
-        <Route path="Me" element={<Me />}/> 
-        <Route path="Works" element={<Works />}/>
-        <Route path="Skillset" element={<Skillset />}/>
-        <Route path="Talk" element={<Talk />}/>
-      </Routes>
+      <div className='app-cont'>
+        <Intro />
+        <Me />
+        <Works />
+        <Skillset />
+        <Talk />
+      </div>
     </div>
   );
 }
